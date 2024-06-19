cc5fa8cfd0be84a08ee7bd91ebc9472cf7461870
yohannes <yonnik446@gmail.com>
2024-06-19 06:30:58 -0700

    test 3

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
-            test2
+            test3
             <Home />
         </View>
     );
